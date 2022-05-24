199f27beb3c03827f70360059445ae74dc5efe79
sugarmouse <tanghao19950911@gmail.com>
2022-05-24 16:09:16 +0800

    更改文件为 ts

---
+++ sort/bubbleSort.ts
@@ -0,0 +1,30 @@
+interface DefaultCompareFn {
+  (a: number, b: number): boolean;
+}
+interface SwitchPlace {
+  (arr: Array<any>, i: number, j: number): void;
+}
+interface BubbleSort {
+  (array: Array<any>, compareFn?: (a: any, b: any) => any):Array<any>;
+}
+
+// 辅助函数
+const defaultCompareFn: DefaultCompareFn = (a: number, b: number): boolean => {
+  return a > b ? true : false;
+
+};
+
+const switchPlace: SwitchPlace = (arr, i, j) => {
+  [arr[j], arr[i]] = [arr[i], arr[j]];
+};
+
+//冒泡排序
+const bubbleSort:BubbleSort = (array, compareFn = defaultCompareFn) => {
+  const { length } = array;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1 - i; j++) {
+      if (compareFn(array[j], array[j + 1])) switchPlace(array, j, j + 1);
+    }
+  }
+  return array;
+};
