b42d17b212147fe1ce36c19de415e55b7ee515d2
sugarmouse <tanghao19950911@gmail.com>
2022-05-25 05:25:27 +0800

    add selectionSort

--- sort/_lib.ts
+++ sort/_lib.ts
@@ -1,16 +1,14 @@
-interface DefaultCompareFn {
+interface IsBigger {
   (a: number, b: number): boolean;
 }
+export const isBigger: IsBigger = (a: number, b: number): boolean => {
+  return a > b ? true : false;
+};
+
+
 interface SwitchPlace {
   (arr: Array<any>, i: number, j: number): void;
 }
-
-
-export const defaultCompareFn: DefaultCompareFn = (a: number, b: number): boolean => {
-  return a > b ? true : false;
-
-};
-
 export const switchPlace: SwitchPlace = (arr, i, j) => {
   [arr[j], arr[i]] = [arr[i], arr[j]];
 };
---
+++ sort/selectionSort.ts
@@ -0,0 +1,20 @@
+import { isBigger, switchPlace } from './_lib.js';
+
+interface SelectionSort {
+  (array: Array<any>, comparaFn?: (a: any, b: any) => boolean): Array<any>;
+}
+
+export const selectionSort: SelectionSort = function (array: Array<any>, comparaFn = isBigger) {
+  const { length } = array;
+  let minIndex;
+  for (let i = 0; i < length - 1; i++) {
+    minIndex = i;
+    for (let j = i; j < length; j++) {
+      if (comparaFn(array[minIndex], array[j])) { minIndex = j; };
+    }
+    if (minIndex !== i) switchPlace(array, minIndex, i);
+  }
+  return array;
+};
+
+console.log(selectionSort([4, 3, 2, 1])); 
