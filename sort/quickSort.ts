75446ed298677868524ee809f1de8659fba4f35f
sugarmouse <tanghao19950911@gmail.com>
2022-05-28 19:46:21 +0800

    新增 快速排序

---
+++ sort/quickSort.ts
@@ -0,0 +1,21 @@
+import { isBigger } from "./_lib.js";
+
+export const quickSort = (array: any[], compareFN = isBigger): any[] => {
+  const { length } = array;
+  if (length <= 1) return array;
+
+  const middle = Math.floor(length / 2);
+  const pivot = array[middle];
+
+  const lesser = [];
+  const greater = [];
+  for (let i = 0; i < length; i++) {
+    if (i === middle) continue;
+    compareFN(array[i], pivot) ? greater.push(array[i]) : lesser.push(array[i]);
+  }
+  const sortedArr = [...quickSort(lesser, compareFN), pivot, ...quickSort(greater, compareFN)];
+  return sortedArr;
+};
+
+
+console.log(quickSort([4, 5, 6, 3, 2, 1]));
